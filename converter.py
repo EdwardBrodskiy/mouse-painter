9d6faa96eaf486a8b193394875758676d7eb5355
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2020-09-11 18:09:44 +0100

    converter fixes

--- converter.py
+++ converter.py
@@ -14,28 +14,34 @@ def convert(img: np.ndarray, size=None):
 
     counter = 0
     prev_color = 0
-    for c in range(0, height - chunk[1], chunk[1]):
-        for r in range(0, width - chunk[0], chunk[0]):
-            color = derive_color_bw(img[r:r + chunk[1], c:c + chunk[0], :])
+    for r in range(0, height - chunk[1], chunk[1]):
+        counter = 0
+        c_counter = 0
+        for c in range(0, width - chunk[0], chunk[0]):
+            section = img[r:r + chunk[1], c:c + chunk[0], :]
+            color = derive_color_bw(section)
             if prev_color != color:
                 instruction_set.append([0, prev_color, counter])
+                c_counter += counter
                 counter = 0
             counter += 1
             prev_color = color
         instruction_set.append([0, prev_color, counter])
-        counter = 0
-        instruction_set.append([0, 0, -size[0]])
+
+        instruction_set.append([0, 0, -(c_counter + counter)])
         instruction_set.append([1, 0, 1])
 
-    display(instruction_set)
+    save(instruction_set)
 
 
 def derive_color_bw(color: np.ndarray):
     return int(127 > color.mean())
 
 
-def display(instructions):
-    print('{')
-    for instruction in instructions:
-        print('{ ' + str(instruction[0]) + ', ' + str(instruction[1]) + ', ' + str(instruction[2]) + ' },')
-    print('}')
+def save(instructions):
+    with open('arduino/main/img.h', 'w') as file:
+        file.write('#define len ' + str(len(instructions)) + '\n')
+        file.write('int path[len][3] = {\n')
+        for instruction in instructions:
+            file.write('{ ' + str(instruction[0]) + ', ' + str(instruction[1]) + ', ' + str(instruction[2]) + ' },\n')
+        file.write('};\n')
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ image_name = "space-invader.jpg"  # input('Enter file name: ')
 
 image_cv2 = cv2.imread(f'imgs/{image_name}')
 
-convert(image_cv2, size=50)
+convert(image_cv2, size=20)
 
-# cv2.imshow("Result display", image_cv2)
+# cv2.imshow("Result save", image_cv2)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
